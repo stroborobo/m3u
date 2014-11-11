4f4885ecab80c3e81286fee65599ac5dc1bd6bef
Björn <bo@kbct.de>
2014-11-11 21:44:56 +0100

    Implement parsing

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 m3u
 ===
 
-A package for writing m3u playlists.
+A package for parsing and writing m3u playlists.
 
 [Documentation online][1]
 
@@ -9,11 +9,6 @@ Installation
 ------------
 
 	go get github.com/Knorkebrot/m3u
-
-ToDo
-----
-
-* Parsing of course. Feel free to add it, I just didn't need it yet.
 
 
 [1]: http://godoc.org/github.com/Knorkebrot/m3u
--- m3u.go
+++ m3u.go
@@ -9,15 +9,19 @@ package m3u
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"bufio"
+	"strings"
+	"strconv"
 )
 
 type Song struct {
-	Length int
+	Length int64
 	Title string
 	Path string
 }
 
-type Playlist []Song
+type Playlist []*Song
 
 func (s Song) String() (string, error) {
 	if len(s.Path) == 0 {
@@ -42,4 +46,40 @@ func (p Playlist) String() (string, error) {
 		b.WriteString(str)
 	}
 	return b.String(), nil
+}
+
+func Parse(r io.Reader) (Playlist, error) {
+	b := bufio.NewReader(r)
+	p := make(Playlist, 0, 10)
+	s := new(Song)
+	for {
+		bytes, err := b.ReadBytes('\n')
+		last := false
+		if err == io.EOF {
+			last = true
+		} else if err != nil {
+			return nil, err
+		}
+		line := strings.TrimSpace(string(bytes))
+		length := len(line)
+		if length >= 7 && line[0:7] == "#EXTINF" {
+			infos := strings.SplitN(line[8:], ",", 2)
+			length, err := strconv.ParseInt(infos[0], 10, 64)
+			if err != nil {
+				return nil, err
+			}
+			s.Length = length
+			s.Title = infos[1]
+		} else if length >= 1 && line[0] == '#' {
+			continue
+		} else if length != 0 {
+			s.Path = line
+			p = append(p, s)
+			s = new(Song)
+		}
+		if last {
+			break
+		}
+	}
+	return p, nil
 }
